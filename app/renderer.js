a87b48215ea801869a7e89e9441587b04c303474
Guy Keller <guy.keller@firstmac.com.au>
2025-11-03 10:52:09 +1000

    MS has changed where the notification pane lives in the DOM, so need to do something a bit more aggressive to capture the notifications displaying in the app

--- app/renderer.js
+++ app/renderer.js
@@ -1,16 +1,59 @@
-// Using this variable as a 'lock' control - 5mins between notifications
+// Using these variable as a 'lock' control - 5mins between notifications
 // So that we don't display the 'notification & pop-up' more than once
 let reminderShowing = false;
+let calendarReminder = false;
 
 function resetReminderFlag() {
     const fiveMins = (5 * 60 * 1000);
     window.setTimeout(() => {
         reminderShowing = false;
+        calendarReminder = false;
     }, fiveMins);
 }
 
 function handleMutation(mutation) {
-    // console.log("Mutation: ", mutation);
+    if (mutation && mutation.addedNodes && mutation.addedNodes.length > 0) {
+        mutation.addedNodes.forEach(newNode => {
+            if (newNode.nodeType === Node.ELEMENT_NODE) {
+                checkForReminderTypeCalendar(newNode);
+                if (calendarReminder) {
+                    showReminder();
+                }
+            }
+        });
+    }
+}
+
+function checkForReminderTypeCalendar(element) {
+    if (!calendarReminder && element) {
+        if (element.children) {
+            for (let i = 0; i < element.children.length; i++) {
+                const child = element.children[i];
+                const isReminder = isCalendarReminder(child);
+                if (isReminder) {
+                    calendarReminder = true;
+                    break;
+                }
+                checkForReminderTypeCalendar(child);
+            }
+        }
+    }
+}
+
+function isCalendarReminder(element) {
+    if (element) {
+        const isReminder = element.hasAttribute("remindertype");
+        if (isReminder) {
+            const reminderValue = element.getAttribute("remindertype");
+            if ("Calendar" === reminderValue) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+function showReminder() {
     if (!reminderShowing) {
         reminderShowing = true;
         window.electronAPI.showOutlookNotification();
@@ -26,8 +69,9 @@ const observer = new MutationObserver(mutations => {
     });
 });
 
-// Watching changes on the 'NotificationPane' div, if something happens there, we have a notification
-const notificationPane = document.querySelector('div[data-app-section="NotificationPane"]');
-if (notificationPane) {
-    observer.observe(notificationPane, {childList: true});
+// Watching changes on the 'TopBar' div, if something happens there, we have a notification
+const bodyElement = document.querySelector('body');
+if (bodyElement) {
+    const obsOpts = {childList: true, subtree: true, attributes: true};
+    observer.observe(bodyElement, obsOpts);
 }
